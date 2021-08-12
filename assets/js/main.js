efaf57dc45bf4890aa5cd473933aa9e7450e0f46
Ella Choy <egal21de@gmail.com>
2021-08-12 15:42:03 +0200

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,21 @@ document.write("Übung lev1_3: data-types");
 // Gebe "typeof()" mit den folgenden Variablen in der Konsole ein, um deren Datentyp zu bestimmen. Welche Datentypen werden Dir angezeigt?
 
 
+let i = 1;
+console.log(typeof(i));
+
+let lastName = "Johnson";
+console.log(typeof(lastName));
+
+let j = "2";
+console.log(typeof(j));
+
+let status=true;
+console.log(typeof(status));
+
+// let hello=”hello”;
+console.log(typeof(hello));
+
+let helloWorld = "hello";
+console.log(typeof(helloWorld));
+
