d353976d79d273a550e30af555dc51f2e22e5f5a
Ella Choy <egal21de@gmail.com>
2021-08-12 15:34:17 +0200

    data-type

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+console.log("hi data-types");
+document.write("Übung lev1_3: data-types");
+
+
+// Gebe "typeof()" mit den folgenden Variablen in der Konsole ein, um deren Datentyp zu bestimmen. Welche Datentypen werden Dir angezeigt?
+
+
